478b1dd74b7da27cff70c4ebe51dd1cd9fd82533
sujit pawar <sujitpawar.scoe.mech@gmail.com>
2025-02-07 14:38:09 +0530

    Fixed next.config.ts image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+    reactStrictMode: true,
+    images: {
+      domains: [
+        "ochi.design", 
+       ] // Replace with actual image domain
+        
+    },
 };
 
 export default nextConfig;
